742ec49284c71cf25bed43d4793d0e87a48a5033
Tjark Ziehm <49223724+MannimMond86@users.noreply.github.com>
2024-04-20 08:46:10 +0200

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,28 @@
+/*H**********************************************************************
+* FILENAME :        main_speedTest.cpp             DESIGN REF: FMCM00
+*
+* DESCRIPTION :
+*       Speed test for the cores of the ESP32. Testing multithreading on ESP32.
+*
+* PUBLIC FUNCTIONS :
+*       void    create_artificial_Load()
+*       int     myFunction(int, int)
+*
+* NOTES :
+*       The naming of the prefixes of the variables and functions results from the variable type or
+*       the variable type of the return value.
+*       "c" stands for "char", an "s" for "short",
+*       "v" for "void", a "u" for "unsigned" and
+*       "x" for all non-standard types
+*
+*       Copyright Tjark Ziehm 2024
+*
+* AUTHOR :    Tjark Ziehm        START DATE :    20 April 2024
+*
+* CHANGES :
+*
+* Convention:   <major>.<minor>.<patch>
+* REF NO  VERSION DATE        WHO     DETAIL
+* F21/33  A.03.04 20April2024 TZ      add second Task for different Core
+*
+*H*/
