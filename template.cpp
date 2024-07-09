e95b65a17eaac2ded3b569b5a905a9ee5d3402d3
Tjark Ziehm <49223724+MannimMond86@users.noreply.github.com>
2024-07-09 10:41:45 +0200

    Update template.cpp
    doc: clean up the Header File

--- template.cpp
+++ template.cpp
@@ -1,28 +1,28 @@
-/*H**********************************************************************
-* FILENAME :        main_speedTest.cpp             DESIGN REF: FMCM00
-* Version  :
-* DESCRIPTION :
-*       Speed test for the cores of the ESP32. Testing multithreading on ESP32.
-*
-* PUBLIC FUNCTIONS :
-*       void    create_artificial_Load()
-*       int     myFunction(int, int)
-*
-* NOTES :
-*       The naming of the prefixes of the variables and functions results from the variable type or
-*       the variable type of the return value.
-*       "c" stands for "char", an "s" for "short",
-*       "v" for "void", a "u" for "unsigned" and
-*       "x" for all non-standard types
-*
-*       Copyright Tjark Ziehm 2024
-*
-* AUTHOR :    Tjark Ziehm        START DATE :    20 April 2024
-*
-* CHANGES :
-*
-* Convention:   <major>.<minor>.<patch>
-* REF NO  VERSION DATE        WHO     DETAIL
-* F21/33  A.03.04 20April2024 TZ      add second Task for different Core
-*
-*H*/
+/*H********************************************************************************************
+** FILENAME :        main.cpp
+** Version  :        1.0.1
+** DESCRIPTION :
+**       Converter from Networktime as a String to integer
+**
+** PUBLIC FUNCTIONS :
+**       int    concat()
+**       void setSerials()
+**       void setupNT-Converter()
+**       int convertNT2Integer()
+**
+** NOTES :
+**      The naming of the prefixes of the variables and functions results from the variable type or
+**       the variable type of the return value.
+**       "c" stands for "char", an "s" for "short",
+**       "v" for "void", a "u" for "unsigned" and
+**       "x" for all non-standard types
+**
+** AUTHOR :            Tjark Ziehm        START DATE :    06 Juli 2024
+** Copyright&Licence:  Tjark Ziehm, 2024
+** Convention:         <major>.<minor>.<patch>
+** CHANGES :
+**            v.0.0.1 add and test functions to read&seperate the char elements in array Networktime
+**            v.1.0.0 add the converter and the concationation function to get as result integers
+**            v.1.1.0 seperate the code to implementable feature
+**
+*********************************************************************************************H*/
